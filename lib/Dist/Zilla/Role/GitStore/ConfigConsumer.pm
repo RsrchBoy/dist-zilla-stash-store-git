10b746f9324535fff4667f7e8fb7014b2aa3f92f
Chris Weyl <cweyl@alumni.drew.edu>
2014-04-29 16:03:22 -0700

    ::Role::GitStore::ConfigConsumer doc

--- lib/Dist/Zilla/Role/GitStore/ConfigConsumer.pm
+++ lib/Dist/Zilla/Role/GitStore/ConfigConsumer.pm
@@ -8,6 +8,14 @@ use MooseX::AttributeShortcuts;
 
 with 'Dist::Zilla::Role::GitStore::Consumer';
 
+=required_method gitstore_config_required
+
+Should return an array of the keys we expect the L<%Store::Git
+stash|Dist::Zilla::Stash::Store::Git> to be able to provide us with values
+for.
+
+=cut
+
 requires 'gitstore_config_required';
 
 !!42;
@@ -16,6 +24,9 @@ __END__
 =head1 SYNOPSIS
 
 =head1 DESCRIPTION
+
+This role should be consumed by something (typically a plugin) that consumes configuration
+information provided by L<%Store::Git|Dist::Zilla::Stash::Store::Git>.
 
 =head1 SEE ALSO
 
