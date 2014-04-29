3cf8491662f870912c638b3b5862c123892bc206
Chris Weyl <cweyl@alumni.drew.edu>
2014-04-29 15:57:04 -0700

    ::Role::GitStore::Consumer doc

--- lib/Dist/Zilla/Role/GitStore/Consumer.pm
+++ lib/Dist/Zilla/Role/GitStore/Consumer.pm
@@ -20,7 +20,19 @@ __END__
 
 =head1 SYNOPSIS
 
+    with 'Dist::Zilla::Role::GitStore::Consumer';
+
+    # ...and elsewhere...
+    $self->_git_store->...
+
 =head1 DESCRIPTION
+
+This role should be consumed by something (typically a plugin) that uses the
+L<%Store::Git stash|Dist::Zilla::Stash::Store::Git>.
+
+Note that this role does not indicate that B<configuration information> is
+being consumed; simply that the consumer uses the store in some way (e.g.
+looking up all tags, querying the repository log, etc).
 
 =head1 SEE ALSO
 
